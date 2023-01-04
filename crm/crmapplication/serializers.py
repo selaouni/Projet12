7ed02cdab1b75c6c6783537c9598c964b20cdd45
sabah <sabah.elaouni@gmail.com>
2023-01-04 01:41:31 +0100

    Serializer creation

---
+++ crm/crmapplication/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Client, Event, Contract, Event_status, Sales, Support
+
+
+
+class ClientSerializer(ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['client_id', 'first_name',
+                  'last_name', 'email',
+                  'phone', 'mobile', 'company_name',
+                  'date_created', 'date_updated',
+                  'sales_contact']
+
+
+class EventSerializer(ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['event_id', 'client','Support_contact',
+                  'date_created', 'date_updated','event_status',
+                  'attendees','event_date','notes']
+
+
+class ContractSerializer(ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = ['contract_id', 'Sales_contact',
+                  'client', 'date_created', 'date_updated',
+                  'status', 'amount',
+                  'payment_due']
+
+
+class EventStatusSerializer(ModelSerializer):
+    class Meta:
+        model = Event_status
+        fields = ['event_status_id', 'Event_status']
+
+
+class SalesSerializer(ModelSerializer):
+    class Meta:
+        model = Sales
+        fields = ['sales_id', 'contact', 'first_name', 'last_name']
+
+
+class SupportSerializer(ModelSerializer):
+    class Meta:
+        model = Support
+        fields = ['support_id', 'contact', 'first_name', 'last_name']
