d58392f854bd323ff0136223449f933628a0a8f7
sabah <sabah.elaouni@gmail.com>
2023-01-04 01:36:54 +0100

    model correction

--- crm/crmapplication/models.py
+++ crm/crmapplication/models.py
@@ -10,6 +10,9 @@ class Support(models.Model):
     first_name = models.fields.CharField(max_length=25)
     last_name = models.fields.CharField(max_length=25)
 
+    # class Meta:
+    #     db_table = 'support'
+    #     verbose_name = 'Support'
 
 class Sales(models.Model):
     sales_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -17,10 +20,21 @@ class Sales(models.Model):
     first_name = models.fields.CharField(max_length=25)
     last_name = models.fields.CharField(max_length=25)
 
+    def __str__(self):
+        return self.first_name
+
+    # class Meta:
+    #     db_table = 'sales'
+    #     verbose_name = 'Equipe commerciale'
+
 
 class Event_status(models.Model):
     event_status_id = models.IntegerField(blank=True)
     Event_status = models.IntegerField(blank=True)
+
+    # class Meta:
+    #     db_table = 'event_status'
+    #     verbose_name = 'Statut des évènement'
 
 
 class Client(models.Model):
@@ -36,10 +50,11 @@ class Client(models.Model):
     sales_contact = models.ForeignKey(to=Sales, on_delete=models.CASCADE)
 
 
+
 class Event(models.Model):
     event_id = models.IntegerField(blank=True)
     client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
-    Support_contact = models.ForeignKey(to=Support, on_delete=models.CASCADE)
+    support_contact = models.ForeignKey(to=Support, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     event_status = models.ForeignKey(to=Event_status, on_delete=models.CASCADE)
@@ -47,17 +62,20 @@ class Event(models.Model):
     event_date = models.DateTimeField(auto_now=True)
     notes = models.TextField(max_length=200, blank=True)
 
+    # class Meta:
+    #     db_table = 'event'
+    #     verbose_name = 'Evènement'
+
 
 class Contract(models.Model):
     contract_id = models.IntegerField(blank=True)
-    Sales_contact = models.ForeignKey(to=Sales, on_delete=models.CASCADE)
+    sales_contact = models.ForeignKey(to=Sales, on_delete=models.CASCADE)
     client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     status = models.BooleanField(blank=True)
     amount = models.FloatField(blank=True)
     payment_due = models.DateTimeField(auto_now=True)
-
 
 
 
