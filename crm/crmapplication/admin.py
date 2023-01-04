84d8b2cec2e00c59027b6f9128e16961a5994d25
sabah <sabah.elaouni@gmail.com>
2023-01-04 01:21:35 +0100

    Updated with admin django customisation

--- crm/crmapplication/admin.py
+++ crm/crmapplication/admin.py
@@ -1,16 +1,58 @@
 from django.contrib import admin
 
-from crmapplication.models import Client
-from crmapplication.models import Contract
-from crmapplication.models import Event
-from crmapplication.models import Sales
-from crmapplication.models import Support
+from .models import Client
+from .models import Contract
+from .models import Event
+from .models import Sales
+from .models import Support
+from .models import Event_status
 
-# Register your models here.
 
-admin.site.register(Client)
-admin.site.register(Contract)
-admin.site.register(Event)
-admin.site.register(Sales)
-admin.site.register(Support)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('client_id', 'first_name', 'last_name', 'email', 'company_name', 'sales_contact')
+    def active(self, obj):
+        return obj.is_active == 1
+
+    active.boolean = True
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_id', 'client', 'Support_contact', 'event_status', 'notes')
+    def active(self, obj):
+        return obj.is_active == 1
+
+    active.boolean = True
+
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('contract_id', 'sales_contact', 'client', 'status', 'payment_due')
+    def active(self, obj):
+        return obj.is_active == 1
+
+    active.boolean = True
+
+
+class SupportAdmin(admin.ModelAdmin):
+    list_display = ('support_id', 'contact','first_name', 'last_name')
+    def active(self, obj):
+        return obj.is_active == 1
+
+    active.boolean = True
+
+
+class SalesAdmin(admin.ModelAdmin):
+    list_display = ('sales_id', 'contact', 'first_name', 'last_name')
+    def active(self, obj):
+        return obj.is_active == 1
+
+    active.boolean = True
+
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Contract, ContractAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Sales, SalesAdmin)
+admin.site.register(Support, SupportAdmin)
+admin.site.register(Event_status)
 
