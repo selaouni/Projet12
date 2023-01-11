7d91d95c76afeb88040854d69454aa7d412a21f4
sabah <sabah.elaouni@gmail.com>
2023-01-11 22:52:29 +0100

    updated with permissions

--- crm/authentication/views.py
+++ crm/authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .serializers import MyTokenObtainPairSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class UserByProjectView(generics.CreateAPIView):
+    queryset = User.objects.all()
