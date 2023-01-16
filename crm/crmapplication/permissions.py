70e7c806f287a22d0a620dadbb0b0383bcf6a9b7
sabah <sabah.elaouni@gmail.com>
2023-01-16 19:28:34 +0100

    Updated

--- crm/crmapplication/permissions.py
+++ crm/crmapplication/permissions.py
@@ -23,20 +23,6 @@ class IsManager(BasePermission):
         return False
 
 
-
-
-class IsOwnerOrReadOnly(BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.author_user_id == request.user
-
 class IsSales(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
@@ -52,13 +38,23 @@ class IsSupport(permissions.BasePermission):
             return True
         return obj.author_user_id == request.user
 
-
-
-class IsSalesOrSupport(permissions.BasePermission):
+class CheckPermission(permissions.BasePermission):
+    edit_methods = ("PUT", "PATCH", "DELETE")
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.user.is_staff:
+            return True
+
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.author_user_id == request.user
+
+        if obj.author_user_id == request.user:
+            return True
+
+        if request.user.is_staff and request.method not in self.edit_methods:
+            return True
+
+        return False
+
