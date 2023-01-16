c9df310002cc96e87e7d87d6a277d0c142e81975
sabah <sabah.elaouni@gmail.com>
2023-01-16 19:35:56 +0100

    Updated

--- crm/authentication/admin.py
+++ crm/authentication/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email')
+
+    def active(self, obj):
+        return obj.is_active == 1
+
+    active.boolean = True
+
+admin.site.register(User,UserAdmin )
