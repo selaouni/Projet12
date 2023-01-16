5bd24981b1e70f0d634abe85c249d415c0898366
sabah <sabah.elaouni@gmail.com>
2023-01-16 19:16:58 +0100

    Urls update

--- crm/authentication/urls.py
+++ crm/authentication/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import RegisterView, UserByProjectView
 
 
 urlpatterns = [
 
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/signup/', RegisterView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
 
     ]
