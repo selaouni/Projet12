0877f51171473648c179ce427b592266b7b9e525
sabah <sabah.elaouni@gmail.com>
2023-01-16 18:48:03 +0100

    update

--- crm/authentication/views.py
+++ crm/authentication/views.py
@@ -4,8 +4,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import User
 from .serializers import RegisterSerializer
 from rest_framework import generics
+import logging
+
+logger = logging.getLogger(__name__)
 
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)s.%(funcName)s +%(lineno)s: %(levelname)-8s [%(process)d] %(message)s',
+                    )
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
