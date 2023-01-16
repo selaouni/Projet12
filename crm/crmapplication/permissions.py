3c5b0b175ef8feabf620b7b076275a401058740c
sabah <sabah.elaouni@gmail.com>
2023-01-16 14:26:56 +0100

    update

--- crm/crmapplication/permissions.py
+++ crm/crmapplication/permissions.py
@@ -1,17 +1,21 @@
 from rest_framework.permissions import IsAdminUser, SAFE_METHODS, BasePermission
 from rest_framework import permissions
-
-#management role
-# class IsAdminUserOrReadOnly(IsAdminUser):
-#
-#     def has_permission(self, request, view):
-#         # is_admin = super(
-#         #     IsAdminUserOrReadOnly,
-#         #     self).has_permission(request, view)
-#         is_admin = super().has_permission(request, view)
-#         return request.method in SAFE_METHODS or is_admin
+from django.contrib.auth.models import Group
 
 
+
+def _is_in_group(user, group_name):
+    """
+    Takes a user and a group name, and returns `True` if the user is in that group.
+    """
+    try:
+        return Group.objects.get(name=group_name).user_set.filter(id=user.id).exists()
+    except Group.DoesNotExist:
+        return None
+
+
+def _has_group_permission(user, required_groups):
+    return any([_is_in_group(user, group_name) for group_name in required_groups])
 class IsManager(BasePermission):
     def has_permission(self, request, view=None):
         if request.user.is_active and request.user.is_staff:
@@ -19,11 +23,7 @@ class IsManager(BasePermission):
         return False
 
 
-# class IsAdminOrStaff(permissions.BasePermission):
-#     message = 'None of permissions requirements fulfilled.'
-#
-#     def has_permission(self, request, view):
-#         return request.user.is_admin() or request.user.is_staff()
+
 
 class IsOwnerOrReadOnly(BasePermission):
     """
@@ -54,3 +54,11 @@ class IsSupport(permissions.BasePermission):
 
 
 
+class IsSalesOrSupport(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.author_user_id == request.user
